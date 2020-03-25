c073129ce0359149a3a75cfd3bc1fb62f1023834
Konstantin Romanov <konstantin.romanov@live.be>
2020-03-25 12:50:51 +0100

    Spring MVC: configuring MVC Controllers, Thymeleaf Templates

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -88,7 +88,6 @@ public class Author {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 }
